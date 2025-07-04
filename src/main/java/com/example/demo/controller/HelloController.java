6d6eddd320787221daeff357ae2c7e9d5b541459
trivedi089 <satyachbs007@gmail.com>
2025-07-04 14:40:44 +0530

    application.yml

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     //@RequestMapping(value="/", method = RequestMethod.GET) we can direct using GetMapping
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome to Request Mapping";
+        return welcomeMessage;
     }
 }
